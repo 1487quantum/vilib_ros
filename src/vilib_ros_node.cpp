f1b8491f925d99170a201b98a2a7ba6457055055
Photon <1487quantum@users.noreply.github.com>
2020-06-10 22:10:14 +0800

    Add vilib fast detector with image publisher

--- src/vilib_ros_node.cpp
+++ src/vilib_ros_node.cpp
@@ -37,15 +37,13 @@ private:
 
 using namespace vilib;
 
-std::shared_ptr<vilib::DetectorBaseGPU> detector_gpu_;
-
 // Frame preprocessing
-#define PYRAMID_LEVELS 1
+#define PYRAMID_LEVELS 6
 #define PYRAMID_MIN_LEVEL 0
 #define PYRAMID_MAX_LEVEL PYRAMID_LEVELS
 
 // FAST detector parameters
-#define FAST_EPSILON (10.0f) //Threshold level
+#define FAST_EPSILON (60.0f) //Threshold level
 #define FAST_MIN_ARC_LENGTH 10
 // Remark: the Rosten CPU version only works with
 //         SUM_OF_ABS_DIFF_ON_ARC and MAX_THRESHOLD
@@ -54,20 +52,20 @@ std::shared_ptr<vilib::DetectorBaseGPU> detector_gpu_;
 // NMS parameters
 #define HORIZONTAL_BORDER 0
 #define VERTICAL_BORDER 0
-#define CELL_SIZE_WIDTH 32
-#define CELL_SIZE_HEIGHT 32
+#define CELL_SIZE_WIDTH 16
+#define CELL_SIZE_HEIGHT 16
 
-std::unordered_map<int, int> pts; //Feature points detected
 // === FEATURE DETECTOR ===
 
 //FASt corner detector fx, return all the points detected
-std::unordered_map<int, int> fDetector(cv::Mat img)
+std::unordered_map<int, int> fDetector(cv_bridge::CvImagePtr imgpt)
 {
-    //For pyramid storage
-    int image_width_ = img.cols;
-    int image_height_ = img.rows;
+    std::shared_ptr<vilib::DetectorBaseGPU> detector_gpu_;
 
-    //std::cout << "Detector (GPU) reset" << std::endl;
+    //For pyramid storage
+    int image_width_ = imgpt->image.cols;
+    int image_height_ = imgpt->image.rows;
+
     detector_gpu_.reset(new FASTGPU(image_width_,
         image_height_,
         CELL_SIZE_WIDTH,
@@ -88,13 +86,10 @@ std::unordered_map<int, int> fDetector(cv::Mat img)
         PYRAMID_LEVELS,
         IMAGE_PYRAMID_MEMORY_TYPE);
 
-    // Create a Frame (image upload, pyramid)
-    std::shared_ptr<Frame> frame0(new Frame(img, 0, PYRAMID_LEVELS));
-    // Reset detector's grid
-    // Note: this step could be actually avoided with custom processing
-    detector_gpu_->reset();
-    // Do the detection
-    detector_gpu_->detect(frame0->pyramid_);
+    
+    std::shared_ptr<Frame> frame0(new Frame(imgpt->image, 0, PYRAMID_LEVELS));	// Create a Frame (image upload, pyramid)
+    detector_gpu_->reset();							// Reset detector's grid (Note: this step could be actually avoided with custom processing)
+    detector_gpu_->detect(frame0->pyramid_);	 				// Do the detection
 
     // Display results
     auto& points_gpu = detector_gpu_->getPoints();
@@ -106,20 +101,16 @@ std::unordered_map<int, int> fDetector(cv::Mat img)
     int qqq = 0; //Index tracker
     for (auto it = points_gpu.begin(); it != points_gpu.end(); ++it) {
         int key = ((int)it->x_) | (((int)it->y_) << 16);
-
         if (key) {
-            //std::cout << "#" << qqq << " , key: " << key << std::endl;
             points_combined.emplace(key, 3);
+            //ROS_WARN_STREAM("#" << qqq << " , key: " << key);
         }
-        else {
-            points_combined.emplace(key, 1);
-        }
-        qqq++;
+        ++qqq;
     }
-    std::cout << "All points: " << points_gpu.size() << ", Valid points: " << points_combined.size() << ", Epsilon: " << FAST_EPSILON << std::endl;
+
     ROS_WARN_STREAM("All points: " << points_gpu.size() << ", Valid points: " << points_combined.size() << ", Epsilon: " << FAST_EPSILON);
-    // Deinitialize the pyramid pool (for consecutive frames)
-    PyramidPool::deinit();
+
+    PyramidPool::deinit();			// Deinitialize the pyramid pool (for consecutive frames)
 
     return points_combined;
 }
@@ -137,36 +128,40 @@ cv::Mat drawText(cv::Mat img, int x, int y, std::string msg)
 //Draw circle
 cv::Mat dCircle(cv::Mat img, int x, int y)
 {
-    int thickness = 1;
+    int thickness = 2;
     cv::circle(img,
         cv::Point(x, y),
         1 * 3 * 1024,
-        cv::Scalar(0, 255, 255),
+        cv::Scalar(0, 255, 0),
         thickness,
         8,
         10);
+    //ROS_WARN_STREAM("CC: " << img.channels(););
+
+    //cv::imshow("cir",img);
+    //      cv::waitKey(5);
+    // cv::destroyWindow("cir");
+
     return img;
 }
 
 //Draw text & detected features on img
-cv::Mat processImg(cv::Mat img, std::unordered_map<int, int> pts)
+cv_bridge::CvImagePtr processImg(cv_bridge::CvImagePtr img, std::unordered_map<int, int> pts)
 {
-    cv::cvtColor(img, img, cv::COLOR_GRAY2BGR); //Convert to
     // draw circles for the identified keypoints
     for (auto it = pts.begin(); it != pts.end(); ++it) {
         int x = (it->first & 0xFFFF) * 1024;
         int y = ((it->first >> 16) & 0xFFFF) * 1024;
         //std::cout << "x: " << x << " , y: " << y << std::endl;
-        cv::Scalar color; // B,G,R
         int thickness = 1;
         if (it->second == 3) {
-            img = dCircle(img, x, y);
+            img->image = dCircle(img->image, x, y);
         }
     }
 
     //Draw text on img
     std::string tPoints = "Corners: " + std::to_string(pts.size());
-    img = drawText(img, 30, 30, tPoints);
+    img->image = drawText(img->image, 30, 30, tPoints);
 
     return img;
 }
@@ -177,22 +172,19 @@ image_transport::Subscriber imgSub;
 void imgCallback(const sensor_msgs::ImageConstPtr& imgp)
 {
     try {
+        std::unordered_map<int, int> pts; //Feature points detected
+
         //http://docs.ros.org/kinetic/api/sensor_msgs/html/image__encodings_8h_source.html
         cv_bridge::CvImagePtr imagePtrRaw = cv_bridge::toCvCopy(imgp, sensor_msgs::image_encodings::BGR16);
-        std::cout << "Left camera: ";
-        //cv::imshow("view", cv_bridge::toCvShare(img, "bgr8")->image);
+        //cv::imshow("view", cv_bridge::toCvShare(`imgp, "bgr16")->image);
         //cv::waitKey(30);
 
-        cv::Mat gImg;
-        cv::cvtColor(imagePtrRaw->image, gImg, cv::COLOR_BGR2GRAY); //Convert to grayscale for detector
+        cv_bridge::CvImagePtr gImg = cv_bridge::toCvCopy(imgp, sensor_msgs::image_encodings::MONO8);	//Create tmp img for detctor
+        pts = fDetector(gImg); 										//Feature detector (FAST) with grayscale img
+        imagePtrRaw = processImg(imagePtrRaw, pts); 							//Draw the feature point(s)on the img/vid
 
-        pts = fDetector(gImg); //Feature detector (FAST) with grayscale img
-        gImg = processImg(gImg, pts); //Draw the feature point(s)on the img/vid
-
-        //sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr16", imagePtrRaw->image).toImageMsg();
-        sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr16", gImg).toImageMsg();
-
-        imgPub.publish(msg);
+        sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr16", imagePtrRaw->image).toImageMsg();
+        imgPub.publish(msg);					//Publish image
     }
     catch (cv_bridge::Exception& e) {
         ROS_ERROR("Could not convert from '%s' to 'bgr16'.", imgp->encoding.c_str());
@@ -204,23 +196,15 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "vilib_ros");
     ros::NodeHandle nh;
 
-    // cv::namedWindow("view", cv::WINDOW_NORMAL);
-    // cv::startWindowThread();
-
     image_transport::ImageTransport it(nh);
 
-    //Publisher
-    imgPub = it.advertise("img_out", 1);
-
-    //Sub
-    imgSub = it.subscribe("img_in", 1, imgCallback);
+    imgPub = it.advertise("img_out", 1); 			//Publisher
+    imgSub = it.subscribe("img_in", 1, imgCallback); 		//Sub
 
     //sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
-    sensor_msgs::ImagePtr msg;
+    //sensor_msgs::ImagePtr msg;
 
     ros::spin();
-    // cv::destroyWindow("view");
 
     return 0;
 }
-
